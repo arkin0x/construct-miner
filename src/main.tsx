e778a523dc068f8ed94957388e39eb13b7fa9210
arkin0x <arkinox@pm.me>
2023-09-01 16:49:28 -0500

    fix scss reference problem

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
-import './scss/app.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
